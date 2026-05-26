99aab9a104afd0bb2d87f239e301076fe510f064
FelipeThePenguin <chrisrockhateseveryone@gmail.com>
2026-05-26 16:46:27 +0800

    Made a route for the orders backend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import productsRouter from './routes/products.js';
 import cartRouter from './routes/cart.js'
+import ordersRouter from './routes/orders.js';
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors({
 app.use(express.json());
 app.use("/products", productsRouter);
 app.use("/cart-items", cartRouter);
+app.use("/orders", ordersRouter);
 
 app.listen(3000, () => {
  console.log("The server is running");
