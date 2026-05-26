0b6f7dcd48fc889a2abbf5191ad4a5bb5494479f
FelipeThePenguin <chrisrockhateseveryone@gmail.com>
2026-05-26 16:41:04 +0800

    Made a route for the cart backend

---
+++ server/routes/cart.js
@@ -0,0 +1,14 @@
+import {ecommerceDatabase} from '../database/connection.js';
+import express from 'express';
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+ const database = await ecommerceDatabase;
+ const collection = await database.collection("cart");
+
+ const result = await collection.find({}, {_id:0}).toArray();
+ res.send(result);
+});
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import productsRouter from './routes/products.js';
+import cartRouter from './routes/cart.js'
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use("/products", productsRouter);
+app.use("/cart-items", cartRouter);
 
 app.listen(3000, () => {
  console.log("The server is running");
